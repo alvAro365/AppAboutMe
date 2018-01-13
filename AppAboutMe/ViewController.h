0ee7be6beabbbab201f1aa134aae780218c6b6b5
Alvar Aronija <Alvar@kadis-air>
2018-01-13 19:52:29 +0100

    Initial Commit

---
+++ AppAboutMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppAboutMe
+//
+//  Created by Alvar Aronija on 13/01/2018.
+//  Copyright © 2018 Alvar Aronija. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
